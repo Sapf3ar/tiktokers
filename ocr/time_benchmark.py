bf83b196f22bda2e825f7600242b94660fbcd4b6
UsefulTornado <Azarel.url@gmail.com>
2024-06-15 13:21:43 +0300

    change output

--- ocr/time_benchmark.py
+++ ocr/time_benchmark.py
@@ -12,19 +12,13 @@ if __name__ == "__main__":
 
     def measure_time_execution(video_link):
         start_time = time.time()
-        texts, num_frames = text_extractor.extract_text(video_link=video_link)
+        texts = text_extractor.extract_text(video_link=video_link)
         unique_texts = list(dict.fromkeys(texts))
         end_time = time.time()
         print(video_link, unique_texts)
-        return (
-            round(end_time - start_time, 2),
-            len(texts),
-            len(unique_texts),
-            num_frames,
-            len(unique_texts) / num_frames,
-        )
+        return (round(end_time - start_time, 2), len(texts), len(unique_texts))
 
-    results = np.array([0.0] * 5)
+    results = np.array([0.0] * 3)
     links = df.sample(num_links, random_state=42).link.tolist()
 
     for link in tqdm(links):
@@ -37,6 +31,4 @@ if __name__ == "__main__":
         f"Average Time: {results[0]}\n"
         f"Average Number of texts: {results[1]}\n"
         f"Average Number of unique texts: {results[2]}\n"
-        f"Average Number of frames: {results[3]}\n"
-        f"Аverage ratio of unique texts to the number of frames: {results[4]}"
     )
