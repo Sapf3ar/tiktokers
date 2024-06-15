e729e5469fe6af0c674ac1571f91cad31c61d0cc
UsefulTornado <Azarel.url@gmail.com>
2024-06-15 13:21:30 +0300

    add exceptions handling

--- ocr/text_extractor.py
+++ ocr/text_extractor.py
@@ -1,5 +1,14 @@
 import cv2 as cv
 import easyocr
+from exceptions import (
+    FilterTextsFromResultError,
+    GetFramesError,
+    GetVideoCaptureError,
+    OCRException,
+    ReadTextFromFramesError,
+    YieldVideoCaptureError,
+)
+from loguru import logger
 from utils import video_capture
 
 
@@ -11,46 +20,55 @@ class TextExtractor:
         self.reader = easyocr.Reader(["ru", "en"], **kwargs)
 
     def _get_frames(self, video_capture, sample_rate, max_frames):
-        fps = video_capture.get(cv.CAP_PROP_FPS)
-        measure_interval = int(fps // sample_rate)
-        num_frames = int(video_capture.get(cv.CAP_PROP_FRAME_COUNT))
-        frame_number = 0
-        frames = []
+        try:
+            fps = video_capture.get(cv.CAP_PROP_FPS)
+            measure_interval = int(fps // sample_rate)
+            num_frames = int(video_capture.get(cv.CAP_PROP_FRAME_COUNT))
+            frame_number = 0
+            frames = []
 
-        while video_capture.isOpened():
-            ret, frame = video_capture.read()
+            while video_capture.isOpened():
+                ret, frame = video_capture.read()
 
-            if not ret or frame_number > num_frames or len(frames) > max_frames:
-                break
+                if not ret or frame_number > num_frames or len(frames) > max_frames:
+                    break
 
-            frame_number += 1
+                frame_number += 1
 
-            if frame_number % measure_interval != 0:
-                continue
+                if frame_number % measure_interval != 0:
+                    continue
 
-            frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-            frames.append(frame)
-        return frames
+                frames.append(frame)
+            return frames
+        except OCRException as exc:
+            raise GetFramesError from exc
 
     def _read_text_from_frames(self, frames, max_image_size, batch_size):
-        return [
-            self.reader.readtext(
-                frame, detail=1, canvas_size=max_image_size, batch_size=batch_size
-            )
-            for frame in frames
-        ]
+        try:
+            return [
+                self.reader.readtext(
+                    frame, detail=1, canvas_size=max_image_size, batch_size=batch_size
+                )
+                for frame in frames
+            ]
+        except OCRException as exc:
+            raise ReadTextFromFramesError from exc
 
     def _get_filtered_text_from_result(self, image_result, confidence_threshold):
-        text = " ".join(
-            [
-                x[self.RESULT_TEXT_IDX]
-                if x[self.RESULT_CONFIDENCE_IDX] > confidence_threshold
-                else ""
-                for x in image_result
-            ]
-        )
-        return text.strip()
+        try:
+            text = " ".join(
+                [
+                    x[self.RESULT_TEXT_IDX]
+                    if x[self.RESULT_CONFIDENCE_IDX] > confidence_threshold
+                    else ""
+                    for x in image_result
+                ]
+            )
+            return text.strip()
+        except OCRException as exc:
+            raise FilterTextsFromResultError from exc
 
     def extract_text(
         self,
@@ -60,22 +78,35 @@ class TextExtractor:
         max_image_size: int = 400,
         max_frames: int = 25,
         batch_size: int = 16,
-    ) -> tuple[list[str], int]:
-        with video_capture(video_link) as cap:
-            frames = self._get_frames(
-                video_capture=cap, sample_rate=sample_rate, max_frames=max_frames
-            )
-
-        result = self._read_text_from_frames(
-            frames=frames, max_image_size=max_image_size, batch_size=batch_size
-        )
+    ) -> list[str]:
         texts = []
 
-        for image_result in result:
-            text = self._get_filtered_text_from_result(
-                image_result=image_result, confidence_threshold=confidence_threshold
-            )
-            if text:
-                texts.append(text)
+        try:
+            with video_capture(video_link) as cap:
+                frames = self._get_frames(
+                    video_capture=cap, sample_rate=sample_rate, max_frames=max_frames
+                )
 
-        return texts, len(frames)
+            result = self._read_text_from_frames(
+                frames=frames, max_image_size=max_image_size, batch_size=batch_size
+            )
+
+            for image_result in result:
+                text = self._get_filtered_text_from_result(
+                    image_result=image_result, confidence_threshold=confidence_threshold
+                )
+                if text:
+                    texts.append(text)
+
+        except GetFramesError:
+            logger.exception("Error when getting frames from video capture")
+        except ReadTextFromFramesError:
+            logger.exception("Error when reading text from frames")
+        except FilterTextsFromResultError:
+            logger.exception("Error when filtering recognized texts")
+        except GetVideoCaptureError:
+            logger.exception("Error when getting video capture from given path")
+        except YieldVideoCaptureError:
+            logger.exception("Error when yielding video capture")
+
+        return texts
--- ocr/utils.py
+++ ocr/utils.py
@@ -1,13 +1,19 @@
 from contextlib import contextmanager
 
 import cv2 as cv
+from exceptions import GetVideoCaptureError, YieldVideoCaptureError
 
 
 @contextmanager
 def video_capture(filepath):
-    cap = cv.VideoCapture(filepath)
-
     try:
-        yield cap
-    finally:
-        cap.release()
+        cap = cv.VideoCapture(filepath)
+    except Exception as exc:
+        raise GetVideoCaptureError from exc
+    else:
+        try:
+            yield cap
+        except Exception as exc:
+            raise YieldVideoCaptureError from exc
+        finally:
+            cap.release()
